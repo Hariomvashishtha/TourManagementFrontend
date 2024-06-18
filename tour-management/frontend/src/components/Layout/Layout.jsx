d4853b65feba5969995048cbfa22a80dd7094219
Hariom Vashishtha <hariom.vashishtha@MB-M-HARIOMV.local>
2024-06-18 21:45:29 +0530

    BACKEND api changes

--- tour-management/frontend/src/components/Layout/Layout.jsx
+++ tour-management/frontend/src/components/Layout/Layout.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Header from "../Header/Header.jsx";
 import Footer from "../Footer/footer.jsx";
 import Routers from '../../routes/Routers.jsx';
+import AlertMessage from '../../pages/Alert.jsx';
 
 const Layout = () => {
+  const [alertMessage, setAlertMessage] = useState('');
+
+  useEffect(() => {
+    // Set the alert message when the component mounts
+    setAlertMessage('Backend is under progress !');
+  }, []);
   return (
     <>
+    <AlertMessage message={alertMessage} />
     <Header />
     <main>
     <Routers />
